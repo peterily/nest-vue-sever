4fb46ef3fd6ec479e13d4929cd295abe0ebfd100
peterily <2749188018@qq.com>
2022-10-19 22:16:08 +0800

    'hhh'

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+// 看啊可能
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
